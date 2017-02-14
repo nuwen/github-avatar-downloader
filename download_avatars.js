3098b21060384ba95544c21fc0a671d5478fd92e
Danton Nguyen <dantonnguyen88@gmail.com>
2017-02-14 16:45:00 +0000

    refactored code

--- download_avatars.js
+++ download_avatars.js
@@ -4,47 +4,35 @@ var args = process.argv.splice(2);
 
 var GITHUB_USER = "nuwen";
 var GITHUB_TOKEN = "a445d3f49931f7005900f78fcdf58b3f014e8d57";
-var FILE_PATH = "./avatars/sdjkfsdjk.jpg";
-var loginData = [];
-var avatarURL = [];
 
+console.log('Welcome to the GitHub Avatar Downloader!');
 
 
 function downloadImageByUrl(url, filePath){
-  console.log("IMAGE RAN");
   request.get(url).pipe(fs.createWriteStream(filePath));
 }
-console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  if(!repoOwner) {
+  if(!repoOwner || !repoName) {
     console.log("Invalid input!");
     return;
   }
 
-  var requestURL = {
+  // Access to Github API requires a "User-Agent" in the headers
+  var options = {
     url: 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
     headers: {
       'User-Agent': 'GitHub Avatar Downloader - Student Project'
     }
   };
-
-  request.get(requestURL)
-         .on('error', function(err) {
-           console.log("ERROR");
-         })
-         .on('response', function(response) {
-           console.log("STATUS CODE: " + response.statusCode);
-         });
-
-  request(requestURL, cb);
+  request(options, cb);
 }
-
 
 
 getRepoContributors(args[0], args[1], function(err, response, body) {
 
   var data = JSON.parse(body);
+
   for(var i = 0; i < data.length; i++){
     var avatarURL = (data[i]['avatar_url']);
     var filePath = ("./avatars/" + data[i]['login'] + ".jpg");
