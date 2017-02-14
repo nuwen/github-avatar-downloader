5738a602627ce7cff6986a32b3205dc10c779635
Danton Nguyen <dantonnguyen88@gmail.com>
2017-02-14 03:17:17 +0000

    added step3

--- README.md
+++ README.md
@@ -13,3 +13,5 @@ This program should be executed from the command line, in the following manner:
 ... (whatever else you'd like to include)
 
 request from https://api.github.com/repos/GITUSER/REPONAME/contributors
+
+9ed0a3253cdee659cb7e3ece6134bed8dc76b0a8
--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,28 @@
 var request = require('request');
 
+
+var GITHUB_USER = "nuwen";
+var GITHUB_TOKEN = "9ed0a3253cdee659cb7e3ece6134bed8dc76b0a8";
+
+
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+
+  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
+  // request(requestURL, (err, response, body) => {
+  //   if (err || response.statusCode !=200) {
+  //     cb(err || "Status code was not 200");
+  //     return;
+  //   }
+  //
+  //   const beers = JSON.parse(body).data;
+  //
+
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("errors:", err);
+  console.log("result:", result);
+});
