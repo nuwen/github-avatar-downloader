f4a5c06bca5303faa2983d593b948ee255e53036
Danton Nguyen <dantonnguyen88@gmail.com>
2017-02-14 04:42:35 +0000

    finally got user-agents to work

--- download_avatars.js
+++ download_avatars.js
@@ -1,28 +1,39 @@
 var request = require('request');
 
-
 var GITHUB_USER = "nuwen";
-var GITHUB_TOKEN = "9ed0a3253cdee659cb7e3ece6134bed8dc76b0a8";
+var GITHUB_TOKEN = "a445d3f49931f7005900f78fcdf58b3f014e8d57";
 
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
-  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
-  // request(requestURL, (err, response, body) => {
-  //   if (err || response.statusCode !=200) {
-  //     cb(err || "Status code was not 200");
-  //     return;
-  //   }
-  //
-  //   const beers = JSON.parse(body).data;
-  //
+  var requestURL = {
+    url: 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {
+      'User-Agent': 'GitHub Avatar Downloader - Student Project'
+    }
+  };
+  request.get(requestURL)
+         .on('error', function(err) {
+           console.log("ERROR");
+         })
+         .on('response', function(response) {
+           console.log("STATUS ERROR: " + response.body);
+         })
+         .on('data', function(data) {
+           console.log(data);
+         });
 
+    // console.log(requestURL);
+    // console.log(body);
 }
+
+
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("errors:", err);
-  console.log("result:", result);
+  console.log("reults:", result);
 });
+
+//
